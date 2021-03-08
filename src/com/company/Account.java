fd7bfff5ad0798fb764757968648dbee59be9459
jaf107 <48786629+jaf107@users.noreply.github.com>
2021-03-08 20:24:09 +0600

    Hashmap header is removed

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -3,7 +3,6 @@ package com.company;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 
 public class Account {
     private String name;
